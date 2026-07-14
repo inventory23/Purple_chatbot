473a4e37145bb4eb0e961d4a68334b8d803a6198
inventory23 <inventory2302@gmail.com>
2026-07-14 15:14:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from chat_engine import ask_llm
+import os
 
 app = Flask(__name__)
 
@@ -43,10 +44,8 @@ def chat():
 
 
 if __name__ == "__main__":
-
     app.run(
-    host="0.0.0.0",
-    port=5000,
-    debug=False,
-    use_reloader=False
-)
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
